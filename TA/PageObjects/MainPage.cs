de9ddec1857d9cdb014d00138da98b9171555b2a
juliademkiv <demkiv109@gmail.com>
2021-09-09 12:25:12 +0300

    Googletests

---
+++ TA/PageObjects/MainPage.cs
@@ -0,0 +1,66 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TA.PageObjects
+{
+   public class MainPage :BasePage
+    {
+
+        public MainPage(IWebDriver driver) : base(driver)
+        {
+
+        }
+        public MainPage()
+        {
+
+        }
+
+        [FindsBy(How = How.XPath, Using = "//div[@class='QFw9Te']/descendant::textarea")]
+        protected IWebElement OriginalTextInputField;
+
+
+        [FindsBy(How = How.XPath, Using = "//span[@class='VIiyi']")]
+        protected IWebElement TranslationField;
+
+        [FindsBy(How = How.XPath, Using = "//*[@class='mqNsCe tQlvad']/descendant::span[@class='TYVfy']")]
+        protected IWebElement HistoryLable;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='ow23']/descendant::i")]
+        protected IWebElement SwapLanguagesBtn;
+
+        public void Search(string searchRequest)
+        {
+            OriginalTextInputField?.Clear();
+            OriginalTextInputField?.Click();
+            OriginalTextInputField?.SendKeys(searchRequest);
+        }
+
+        public string GetTtanslationText()
+        {
+            return TranslationField?.Text;
+        }
+        
+        public HistoryPage GetHistoryPage()
+        {
+            HistoryLable?.Click();
+            return new HistoryPage();
+        }
+        
+        public void SwapLangueges()
+        {
+            SwapLanguagesBtn?.Click();
+        }
+
+        public bool Verification(int testpass)
+        {
+            if (testpass == 1)
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
